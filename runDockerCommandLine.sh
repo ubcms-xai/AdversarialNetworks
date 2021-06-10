fa745344768dcdd60c664e2a7bb122a45b94aa45
Margaret Morris <megmorris121@gmail.com>
2021-06-10 10:43:21 -0400

    Simplifying command script

--- runDockerCommandLine.sh
+++ runDockerCommandLine.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-​
-ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
-xhost + $ip
-docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -v ${PWD}:/home/physicist/ -e DISPLAY=$ip:0 -p $1:8888 --entrypoint /bin/bash $2
+docker run --rm -it -v ${PWD}:${PWD} -w ${PWD} -p $1:8888 --entrypoint /bin/bash $2
